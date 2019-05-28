06ecdc223d8608f2a352c251a73c4f2c45e48ddd
大蔵海斗 <tobiuo@ookurakaitonoMacBook-Pro.local>
2019-05-29 02:23:03 +0900

    create coin.py

---
+++ coin.py
@@ -0,0 +1,62 @@
+'''
+>>> import coin
+>>> c = coin.Coin()
+>>> print(c.face)
+表
+>>> print(c.num)
+0
+>>> c.conversion(1)
+'裏'
+>>> c.conversion(3)
+'裏'
+>>> c.conversion(0)
+'表'
+>>> c.conversion(2)
+'表'
+>>> c.toss()
+>>> print(c.face)
+表
+>>> print(c.num)
+0
+'''
+
+"""
+
+"""
+
+import random
+
+class Coin:
+    """
+    コインのクラス.
+    ディーラーがコイントスする際に使うコインと,
+    プレイヤーがコイントスの結果を予想するときに使うコインの両方で使う.
+    """
+    face = '表' #表裏を示す.
+    num = 0 #表裏を示す.偶数は表,奇数は裏に対応している.
+
+    def __init__(self):
+        self.num = 0
+        self.face = self.conversion(self.num)
+
+    def conversion(self, num):
+        """
+        numを引数にとって表裏で返す.変換器.
+        """
+        face = None
+        if num % 2 == 0:
+            face = '表' #偶数なら表.
+        else:
+            face = '裏' #奇数なら表.
+        return face
+
+    def toss(self):
+        """
+        コイントスをする関数.
+        """
+        self.num = random.randint(0, 1) #0か1の値を取る.
+        self.face = self.conversion(self.num)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
