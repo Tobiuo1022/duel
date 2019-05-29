b5aa14b5e1daa2ab149a7f042246b47018818f0c
大蔵海斗 <tobiuo@ookurakaitonoMacBook-Pro.local>
2019-05-29 16:50:35 +0900

    add CallOrFold() and fix betting() in player.py

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import coin
+import dealer
 
 class Player:
     playerNo = 0 #プレイヤーの番号.
@@ -8,16 +9,32 @@ class Player:
     bet = 0 #賭け金
     # predict = 0 #予想した面.
     coin = coin.Coin()
-    bluff = 0 #通常は0,嘘をつくと1が入る.
+    bluff = 0 #通常は偶数,嘘をつくと奇数が入る.
     douted = 0 #ダウト先のプレイヤー.
 
     def betting(self):
+        """
+        コイントスの結果を予想して所持金を賭ける関数.
+        """
         print('どちらに賭けますか？(表, 裏)')
         self.coin.num = self.answer('表', '裏') #入力
         self.coin.face = self.coin.conversion(self.coin.num)
         print('いくら賭けますか？')
         self.bet = self.inputInt() #入力
-        print(self.coin.face, self.bet)
+        self.money -= self.bet
+        print(self.coin.face, self.bet) #出力確認用
+
+    def CallOrFold(self, dealer):
+        """
+        コイントスの結果と予想を判定して、降りるか降りないかを決める関数.
+        """
+        if self.coin.num == dealer.coin.num: #プレイヤーとディーラーのコインが一致してるかの判定
+            print('予想が的中しました.(降りる, 降りない)')
+            self.bluff = self.answer('降りる', '降りない') + 1
+        else:
+            print('予想が外れました.(降りる, 降りない)')
+            self.bluff = self.answer('降りる', '降りない')
+        print(self.bluff) #出力確認用
 
     def answer(self, zero, first):
         """
